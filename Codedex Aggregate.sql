ac0673d1f7dd0a062b160fcb36db50e46123a563
Ha NGUYEN <nganha93@gmail.com>
2024-06-05 16:01:08 +0700

    Create Codedex Aggregate.sql

---
+++ Codedex Aggregate.sql
@@ -0,0 +1,10 @@
+--1. SUM()
+SELECT SUM(columnn)
+FROM table_name;
+--2. AVG() : Average
+SELECT AVG(columnn)
+FROM table_name;
+--3.GROUP BY: The GROUP BY statement groups rows of data with the same values into buckets. It’s often used with aggregate functions to group the result by one or more columns.
+SELECT *
+FROM table_name
+GROUP BY columnn;
