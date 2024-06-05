6ca7f91abfe43dc7bb2a4c3aaf66a54140c88ebc
Ha NGUYEN <nganha93@gmail.com>
2024-06-05 21:00:31 +0700

    Update Basic SELECT.sql

--- Basic SELECT.sql
+++ Basic SELECT.sql
@@ -6,31 +6,37 @@
   SELECT column1, column2, column3
   FROM table_name;
 -- Select distinct values:
-  SELECT DISTINCT columnn
+  SELECT DISTINCT column_n
   FROM table_name;
 -- Select with condition:
   SELECT *
   FROM table_name;
-  WHERE column...;
-Comparison operators: =, !=, >, <, >=, <=
+  WHERE column_n...;
+/*Comparison operators: =, !=, >, <, >=, <=
+Conditions are combined by AND/OR*/
 --2. LIKE: The LIKE operator can be used to search for a pattern in a column. It’s used in the WHERE clause
   SELECT * 
   FROM table_name 
-  WHERE columnn LIKE '%...%';
+  WHERE column_n LIKE '%...%';
 --3. BETWEEN: The BETWEEN operator is used in a WHERE clause to filter the result set within a certain range. The range must be separated by an AND keyword
   SELECT * 
   FROM table_name 
-  WHERE columnn
+  WHERE column_n
   BETWEEN value1 AND value2;
 --4. ORDER BY: 
   SELECT * 
   FROM table_name
-  ORDER BY columnn DESC/ASC
+  ORDER BY column_n DESC/ASC
 --5. COUNT:
   SELECT COUNT(*)
   FROM table_name;
 --6. MIN & MAX:
   SELECT column1, column2,
-  MIN/MAX(columnn)
+  MIN/MAX(column_n)
   FROM table_name;
+--7. IN: specify a list of values that you'd like to include in the results
+  SELECT * 
+  FROM table_name
+  WHERE coulmn_n IN (value1, value2, value3,...value_n);
+
 
