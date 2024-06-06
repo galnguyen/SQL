498ffdf3eb2d745156a3b1c080dc5a69680f3d47
Ha NGUYEN <nganha93@gmail.com>
2024-06-06 13:10:06 +0700

    Update Advanced.sql

--- Learning/Advanced.sql
+++ Learning/Advanced.sql
@@ -1,4 +1,10 @@
 --1. CAST: change data type
 SELECT CAST(column1 AS datatype)
 FROM table_name;
---2.Date/time format:
+--2.Date/time format: YYYY-MM-DD
+--Cast a field as TIMESTAMP: 
+SELECT column_n::timestamp
+FROM table_name;
+--INTERVAL: The interval is defined using plain-English terms like '10 seconds' or '5 months'. Also note that adding or subtracting a date column and an interval column results in another date column
+SELECT column_n::timestamp + INTERVAL '2 weeks'
+FROM table_name;
